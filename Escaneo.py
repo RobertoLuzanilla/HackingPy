8a963f79b89a8c0cd8c2ce541590533cb5f6a7bf
luzan <luzanillasanchez@gmail.com>
2025-02-10 07:51:01 -0700

    Add detailed ping functionality and IP validation to Escaneo.py

--- Escaneo.py
+++ Escaneo.py
@@ -1,4 +1,26 @@
 import socket
+import ping3
+import time
+import ipaddress
+
+def verificar_IP(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def ping_Detallado(ip):
+    for i in range(4):
+        time.sleep(3)
+        try: 
+            ping = ping3.ping(ip) * 1000  # Multiplicamos por 1000 para obtener el tiempo en milisegundos
+            if ping is None:
+                print(f"{i}- No se pudo obtener el ping a la IP {ip}")
+            else:
+                print(f"{i}- El ping a la IP {ip} es de: {ping:.2f} ms")
+        except Exception as e:
+            print(f"{i}- Error al hacer ping a la IP {ip}: {e}")
 
 def escaneo_Rapido(ip, puerto):
     print(f"Escaneando IP: {ip} En los puertos: {puerto}")
@@ -29,19 +51,28 @@ def escaneo_Rapido(ip, puerto):
 def seleccion_Tipos():
         while True:
             print("\nSelecciona una opcion\n")
-            print("1-Escaneo de puertos rapido --Escaneo Rapido, verifica los puertos mas escenciales\n")
-            print("2-Escaneo de puertos completo --Escaneo Completo, verifica todos los puertos\n")
+            print("1-Ping --Verifica si hay conexion activa a la IP\n")
+            print("2-Escaneo de puertos rapido --Escaneo Rapido, verifica los puertos mas escenciales\n")
+            print("3-Escaneo de puertos completo --Escaneo Completo, verifica todos los puertos\n")
             print("-----Mas opciones pronto-----\n")
             print("0-Salir")
             opcion = int(input())
 
             if opcion == 1:
+                print("Ping")
+                ip_objetivo = input("Introduce la IP a escanear:")
+                if verificar_IP(ip_objetivo):
+                    ping_Detallado(ip_objetivo)
+                else:
+                    print("La IP no es valida")
+            elif opcion == 2: 
                 print("Escaneo Rapido")
                 ip_objetivo = input("Introduce la IP a escanear: ")     
                 puertos_a_escanear = [21,22,23,25,53,80,110,143,443,631,3389,3306,5432,5900,8080,9200]
                 escaneo_Rapido(ip_objetivo, puertos_a_escanear)
-            elif opcion == 2: 
+            elif opcion == 3:
                 print("Escaneo Completo")
+                print("----Pronto----")
             elif opcion == 0:
                 print("Saliendo...")
                 print("Gracias ;)")
