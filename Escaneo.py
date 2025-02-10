01e4d31c67c07b1ccefd04c3caceb4b80bd1607f
luzan <luzanillasanchez@gmail.com>
2025-02-10 14:57:41 -0700

    Refactor Escaneo.py to add logging functionality and improve input validation; update menu in Menu.py for better user experience

--- Escaneo.py
+++ Escaneo.py
@@ -3,29 +3,42 @@ import ping3
 import time
 import ipaddress
 
-def verificar_IP(ip):
+def escribir_Log(mensaje, archivo): #Funcion para escribir en un archivo de texto
+    with open(archivo, "a") as archivo:
+        archivo.write(mensaje)
+
+def verificar_IP(ip): #Verifica si la ip que se introduce es valida
     try:
         ipaddress.ip_address(ip)
         return True
     except ValueError:
         return False
 
-def ping_Detallado(ip):
+def ping_Detallado(ip): #Lanza un ping a la ip que se introduce
     for i in range(4):
-        time.sleep(3)
+        time.sleep(3) #Hace que espere 3 segundos antes de hacer el siguiente ping
+        mensaje = f"{i}- Haciendo ping a la IP {ip}\n" #Mensaje que se guardara en el archivo de texto
+        escribir_Log(mensaje, "ping_Detallado.txt")
         try: 
             ping = ping3.ping(ip) * 1000  # Multiplicamos por 1000 para obtener el tiempo en milisegundos
             if ping is None:
                 print(f"{i}- No se pudo obtener el ping a la IP {ip}")
+                mensaje = f"{i}- No se pudo obtener el ping a la IP {ip}\n" #Mensaje que se guardara en el archivo de texto
+                escribir_Log(mensaje, "ping_Detallado.txt")
             else:
                 print(f"{i}- El ping a la IP {ip} es de: {ping:.2f} ms")
+                mensaje = f"{i}- El ping a la IP {ip} es de: {ping:.2f} ms\n" #Mensaje que se guardara en el archivo de texto
+                escribir_Log(mensaje, "ping_Detallado.txt")
+
         except Exception as e:
             print(f"{i}- Error al hacer ping a la IP {ip}: {e}")
+            mensaje = f"{i}- Error al hacer ping a la IP {ip}: {e}\n" #Mensaje que se guardara en el archivo de texto
+            escribir_Log(mensaje, "ping_Detallado.txt")
 
-def escaneo_Rapido(ip, puerto):
+def escaneo_Rapido(ip, puerto):#Un escaneo a los puertos mas comunes
     print(f"Escaneando IP: {ip} En los puertos: {puerto}")
-    with open("escan_Rapido.txt", "a") as archivo:
-            archivo.write(f"Escaneo en la IP {ip}\n")#Registra la ip en el archivo
+    mensaje = f"Escaneando IP: {ip} En los puertos: {puerto}\n" #Mensaje que se guardara en el archivo de texto
+    escribir_Log(mensaje, "escan_Rapido.txt")
 
     for i in puerto:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,28 +48,32 @@ def escaneo_Rapido(ip, puerto):
 
         if result == 0:
             print(f"[+] Puerto {i} esta abierto")
-            with open("escan_Rapido.txt", "a") as archivo:
-                archivo.write(f"El puerto {i} esta abierto\n")
+            mensaje = f"El puerto {i} esta abierto\n" #Mensaje que se guardara en el archivo de texto
+            escribir_Log(mensaje, "escan_Rapido.txt")
         
         else:
             print(f"[-] Puerto {i} esta cerrado")
-            with open("escan_Rapido.txt", "a") as archivo:
-                archivo.write(f"El puerto {i} esta cerrado\n")
+            mensaje = f"El puerto {i} esta cerrado\n" #Mensaje que se guardara en el archivo de texto
+            escribir_Log(mensaje, "escan_Rapido.txt")
 
         s.close()
 
     print("Escaneo finalizado\n,\n")
 
 
-def seleccion_Tipos():
+def seleccion_Tipos(): #Menu de la clase Escaneo
         while True:
             print("\nSelecciona una opcion\n")
-            print("1-Ping --Verifica si hay conexion activa a la IP\n")
-            print("2-Escaneo de puertos rapido --Escaneo Rapido, verifica los puertos mas escenciales\n")
-            print("3-Escaneo de puertos completo --Escaneo Completo, verifica todos los puertos\n")
+            print("1-Ping --Verifica si hay conexion activa a la IP\n") #Verifica si hay conexion
+            print("2-Escaneo de puertos rapido --Escaneo Rapido, verifica los puertos mas escenciales\n") #Escaneo de puertos
+            print("3-Escaneo de puertos completo --Escaneo Completo, verifica todos los puertos\n")#Escaneo de puertos PRONTO
             print("-----Mas opciones pronto-----\n")
             print("0-Salir")
-            opcion = int(input())
+            try:
+                opcion = int(input("Introduce una opción: "))
+            except ValueError:
+                print("Por favor, introduce un número entero.")
+            continue
 
             if opcion == 1:
                 print("Ping")
@@ -67,9 +84,12 @@ def seleccion_Tipos():
                     print("La IP no es valida")
             elif opcion == 2: 
                 print("Escaneo Rapido")
-                ip_objetivo = input("Introduce la IP a escanear: ")     
-                puertos_a_escanear = [21,22,23,25,53,80,110,143,443,631,3389,3306,5432,5900,8080,9200]
-                escaneo_Rapido(ip_objetivo, puertos_a_escanear)
+                ip_objetivo = input("Introduce la IP a escanear: ")   
+                if  verificar_IP(ip_objetivo):
+                        puertos_a_escanear = [21,22,23,25,53,80,110,143,443,631,3389,3306,5432,5900,8080,9200]#Puertos comunes a escanear
+                        escaneo_Rapido(ip_objetivo, puertos_a_escanear)
+                else:
+                    print("La IP no es valida") 
             elif opcion == 3:
                 print("Escaneo Completo")
                 print("----Pronto----")
@@ -80,5 +100,5 @@ def seleccion_Tipos():
             else :
                 print("Opcion no valida")
 
-
-seleccion_Tipos()
+if __name__ == "__main__": #Para que se ejecute el menu
+    seleccion_Tipos()
--- Menu.py
+++ Menu.py
@@ -1,15 +1,18 @@
 # Autor: Roberto Luzanilla
+import Escaneo
+
 def menu():            
-    while True:
-        print("\n1- Escaneo de puertos\n")
-        print("-----Más opciones pronto-----\n")
+    while True: #En este bucle se ejecutará el menú, donde escogeremos la opcion que deseamos
+        print("\n1- Escaneo de puertos\n") #Manda a otro archivo con mas opciones
+        print("-----Más opciones pronto-----\n") #Agregar un encriptador de contraseas
         print("0- Salir\n")
-        print("Versión 1.0")
-        
-        opcion = int(input("Introduce una opción: "))
-        
+        print("Versión 1.1")
+        try:
+            opcion = int(input("Introduce una opción: "))
+        except ValueError:
+            print("Por favor, introduce un número entero.")
+            continue
         if opcion == 1:
-            import Escaneo
             Escaneo.seleccion_Tipos()
         elif opcion == 0:
             print("Gracias por usar mis herramientas")
@@ -17,6 +20,7 @@ def menu():
         else:
             print("Opción no válida, por favor ingresa una opción válida.")
 
-print("Hola, bienvenido a mi paquete de herramientas. Aquí podrás encontrar varias herramientas")
+#Este es un mensaje de bienvenida
+print("\nHola, bienvenido a mi paquete de herramientas. Aquí podrás encontrar varias herramientas")
 print("que he creado para mi aprendizaje. Espero que te sirvan estas herramientas.\nSi tienes alguna sugerencia o mejora, no dudes en decírmelo.")
 menu()
